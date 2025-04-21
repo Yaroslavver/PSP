6aac6b8c78206b3109d1772291623dd5217f62ab
Yaroslavver <103107983+Yaroslavver@users.noreply.github.com>
2025-04-21 11:29:54 +0300

    папка компонентов

---
+++ components/product-card/index.js
@@ -0,0 +1,36 @@
+export class ProductCardComponent {
+    constructor(parent) {
+        this.parent = parent;
+    }
+
+    getHTML(data) {
+        return (
+            `
+                <div class="card">
+                    
+                    <div class="card-body">
+                    <button class="btn btn-primary" id="click-card-${data.id}" data-id="${data.id}">Подробнее</button>
+                        <h1 class="card-title">${data.title}</h1>
+                        <p class="card-text">${data.text.length > 90 ? data.text.substring(0, 90)+'...' : data.text}</p>
+                        
+                    </div>
+                    <img class="card-img-top" src="${data.src}" alt="картинка">
+                </div>
+            `
+        )
+    }
+    
+    addListeners(data, listener) {
+        document
+            .getElementById(`click-card-${data.id}`)
+            .addEventListener("click", listener)
+    }
+
+    render(data, listener) {
+        const html = this.getHTML(data)
+        this.parent.insertAdjacentHTML('beforeend', html)
+        this.addListeners(data, listener)
+    }
+    
+    
+}
