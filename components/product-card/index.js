24d5c1537a8c3a6b4a41c087518b66d3fbb1ae60
Yaroslavver <103107983+Yaroslavver@users.noreply.github.com>
2025-05-25 17:31:01 +0300

    Update index.js

--- components/product-card/index.js
+++ components/product-card/index.js
@@ -10,6 +10,7 @@ export class ProductCardComponent {
                     <div class="card-body">
                     <button class="btn btn-primary" id="click-card-${data.id}" data-id="${data.id}">Подробнее</button>
                     <button class="btn btn-primary" id="delete-card-${data.id}" data-id="${data.id}">✕</button>
+                    <button class="btn btn-primary" id="edit-card-${data.id}" data-id="${data.id}">Изменить</button>
                         <h1 class="card-title">${data.title}</h1>
                         <p class="card-text">${data.text.length > 90 ? data.text.substring(0, 90)+'...' : data.text}</p>
                         
@@ -21,20 +22,22 @@ export class ProductCardComponent {
         )
     }
     
-    addListeners(data, listener, listener2) {
+    addListeners(data, listener, listener2, listn_edit) {
         document
             .getElementById(`click-card-${data.id}`)
             .addEventListener("click", listener)
         document
         .getElementById(`delete-card-${data.id}`)
         .addEventListener('click', listener2);
-
+        document
+        .getElementById(`edit-card-${data.id}`)
+        .addEventListener('click', listn_edit);
     }
 
-    render(data, listener, listener2) {
+    render(data, listener, listener2, listn_edit) {
         const html = this.getHTML(data)
         this.parent.insertAdjacentHTML('beforeend', html)
-        this.addListeners(data, listener, listener2)
+        this.addListeners(data, listener, listener2, listn_edit)
     }
     
     
